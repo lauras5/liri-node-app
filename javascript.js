818fa4434e35838031b7617faf3515d863742609
Laura Salcido <idlaurasalcido@gmail.com>
2018-03-24 10:15:42 -0700

    added new content

---
+++ javascript.js
@@ -0,0 +1,6 @@
+var request = require('request');
+var twitter = require('twitter')
+var spotify = require('spotify')
+
+var spotify = new Spotify(keys.spotify);
+var client = new Twitter(keys.twitter);
---
+++ liri.js
@@ -0,0 +1,4 @@
+require("dotenv").config();
+
+//my-tweets will show my last 20 tweets
+console.log(process.env.consumer_key)
