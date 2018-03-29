4066e55db5ef7e6ab9b14c80a630045633dd101a
Laura Salcido <33909888+lauras5@users.noreply.github.com>
2018-03-29 15:58:07 -0700

    Delete javascript.js

--- javascript.js
+++
@@ -1,7 +0,0 @@
-require("dotenv").config();
-
-var request = require('request');
-var twitter = require('twitter')
-
-var spotify = new Spotify(keys.spotify);
-var client = new Twitter(keys.twitter);
